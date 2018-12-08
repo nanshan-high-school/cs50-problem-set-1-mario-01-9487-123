333b637bdf453dad8bc11fa76f60ccb5aeb46754
25127高廷睿 <43640542+9487-123@users.noreply.github.com>
2018-12-08 10:53:58 +0800

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -2,19 +2,29 @@
 using namespace std;
 int main() 
 {
-  int a,b,c;
-  cout <<"請輸入運算符號:(1:+,2:-,3:*,4:/)";
-  cin >> a;
-  cout <<"請輸入第一個數字:";
-  cin >> b;
-  cout <<"請輸入第二個數字:";
-  cin >> c;
-  if (a==1)
-    cout << b+c;
-  if (a==2)
-    cout << b-c;
-  if (a==3)
-    cout << b*c;
-  if (a==4)
-    cout << b/c;
+int a,b,c;
+cout <<"請輸入運算符號:(1:+,2:-,3:*,4:/)";
+cin >> a;
+if (a<1 || a>4)
+{cout <<"請輸入1,2,3,4";return 0;}
+cout <<"請輸入第一個數字:";
+cin >> b;
+cout <<"請輸入第二個數字:";
+cin >> c;
+cout << "運算結果為:";
+if (a==1)
+{cout << b+c;}
+if (a==2)
+{cout << b-c;}
+if (a==3)
+{cout << b*c;}
+if (a==4)
+{cout << b/c;}
+int d;
+cout <<"\n請輸入運算結果:";
+cin >> d;
+if (d%3==0)
+{cout <<"是3的倍數";}
+if (d%3!=0)
+{cout <<"不是3的倍數";}
 }
