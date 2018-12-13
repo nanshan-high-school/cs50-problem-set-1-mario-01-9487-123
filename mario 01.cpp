c96e2030cd0384bbd1468831cac95fae1d8c5366
25127高廷睿 <43640542+9487-123@users.noreply.github.com>
2018-12-13 21:50:39 +0800

    Update mario 01.cpp

--- mario 01.cpp
+++ mario 01.cpp
@@ -2,19 +2,17 @@
 using namespace std;
 
 int main(){
-  int height;
-  cout <<"請輸入1-8的一個數字:";
-  cin >> height;
-  if(height<1 || height>8)
-    cout<<"不再範圍內";
-  else
-    for(int x=1; x<height+1; x++){
-      for(int y=height; y>x; y--){
-        cout<< " ";
-      }
-      for(int y=1; y<=x; y++){
-        cout<< "#";
-      }
-      cout<< "\n";
-    }
+int height;
+do
+{
+cout <<"請輸入1-8的一個數字:";
+cin >> height;
+}
+while(height<1 || height>8);
+for(int x=1; x<height+1; x++){
+for(int y=height; y>x; y--){
+cout<< " ";}
+for(int y=1; y<=x; y++){
+cout<< "#";}
+cout<< "\n";}
 }
